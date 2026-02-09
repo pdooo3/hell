a51f9c09c260f137dd1e07b85a5610ea3d8cb404
mahdi <mahdi.ma.0919@gmail.com>
2026-02-09 23:57:50 +0330

    v1

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  // output: 'export', // <- این خط رو حذف کن
+  // basePath: '/something', // <- این خط رو حذف کن
 };
 
 export default nextConfig;
